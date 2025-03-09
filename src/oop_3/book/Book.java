468e6f965c0d84c91f6a0c1d0b15947585692eec
Umarbekdeveloper <umarbekbahromov07@gmail.com>
2025-03-09 22:28:47 +0500

    added oop_3

---
+++ src/oop_3/book/Book.java
@@ -0,0 +1,72 @@
+package oop_3.book;
+
+import java.util.Arrays;
+
+public class Book{
+    private String name;
+    private Author[] authors;
+    private double price;
+    private int qty = 0;
+
+    public Book(String name, Author[] authors, double price) {
+        this.name = name;
+        this.authors = authors;
+        this.price = price;
+    }
+
+    public Book(String name, Author[] authors, double price, int qty) {
+        this.name = name;
+        this.authors = authors;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author[] getAuthors() {
+        return authors;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder authorDetails = new StringBuilder();
+        for (Author author : authors) {
+            authorDetails.append(author.toString()).append(", ");
+        }
+        return "Book [" +
+                "name='" + name + '\'' +
+                ", authors={" + authorDetails +"}"+
+                ", price=" + price +
+                ", qty=" + qty +
+                ']';
+    }
+
+    public String getAuthorNames(){
+        StringBuilder names = new StringBuilder();
+        for (int i = 0; i < authors.length; i++) {
+            names.append(authors[i].getName());
+            if (i < authors.length - 1) {
+                names.append(", ");
+            }
+        }
+        return names.toString();
+    }
+}
